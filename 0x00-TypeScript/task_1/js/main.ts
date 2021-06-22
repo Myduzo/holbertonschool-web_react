6300bdc216767bc27335422ab790dcbd86f0a666
Myduzo <sahli.youssef@outlook.com>
2021-06-22 22:44:27 +0100

    task 1

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -1,9 +1,10 @@
-// Task 1
+/* Task 1 */
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
+  location: string;
   [propName: string]: any;
 }
 
