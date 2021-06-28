b599ee9e673444cd50b36688298e6147bd18bc05
Myduzo <sahli.youssef@outlook.com>
2021-06-28 15:43:01 +0100

    task 5

---
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -0,0 +1,51 @@
+/* TASK 5 */
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+export class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+export class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+export function createEmployee(salary: number | string) {
+  if (typeof salary === 'number' && salary < 500) return new Teacher();
+  return new Director();
+}
+
+/* TEST FOR TASK 5
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
+*/
