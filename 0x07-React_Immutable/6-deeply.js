0f466307f6da415a0c017b7f40ea6319e4a34355
Myduzo <sahli.youssef@outlook.com>
2021-07-28 16:36:16 +0100

    Nested merge

--- 0x07-React_Immutable/6-deeply.js
+++ 0x07-React_Immutable/6-deeply.js
@@ -1,5 +1,5 @@
-import { fromJS } from 'immutable';
+import { Map } from 'immutable';
 
 export const mergeDeeplyElements = (page1, page2) => {
-  return fromJS(page1).mergeDeep(fromJS(page2));
+  return Map(page1).mergeDeep(Map(page2));
 }
